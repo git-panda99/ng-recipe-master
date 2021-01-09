fba8cacfb5b85100bc5dc816a426cc96429c7be2
paula-christine <paula.ciorba@gmail.com>
2021-01-09 11:33:05 +0200

    RM-12.1 Added Categories only for admin

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/shared/services/auth.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+  
+  isAdmin(): number{
+    return this.authService.userData.email.localeCompare(new String("paula.ciorba@gmail.com"));
   }
 
 }
